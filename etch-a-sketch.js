d3bfc9fafef6db1b210bed8d06b27d593d7805f8
coderlore <64398168+coderlore@users.noreply.github.com>
2021-04-25 14:46:12 -1000

    Created 16x16 grid. Added event listeners for mouseover. Added event listeners to clear grid

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,4 +1,53 @@
+const container = document.getElementById("container");
+const button = document.querySelectorAll('button');
+
+let row = document.getElementsByClassName("row");
+let col = document.getElementsByClassName("col");
+
 function grid() {
+    makeRows(16);
+    makeColumns(16);
+}
+
+function makeRows(rowNum) {
+    // Creates rows
+    for (x = 0; x < rowNum; x++) {
+        let row = document.createElement("div");
+        container.appendChild(row).className = "row";
+    };
+};
+
+function makeColumns(colNum) {
+    for (let y = 0; y < row.length; y++) {
+        for (z = 0; z < colNum; z++) {
+            let newCol = document.createElement("div");
+            row[z].appendChild(newCol).className = "col";
+        };
+    };
+};
+
+// Changes the background color 
+function changeColor (target) {
+    target.style.backgroundColor = "red";
+}
+
+container.addEventListener("mouseover", function (e) {
+    target = e.target;
+
+    if (target.matches("div.col")) {
+        changeColor(target);
+    }
+});
+
+// Removes the background color
+const clear = document.querySelector('#clear');
+clear.addEventListener('click', function() {
+    window.location.reload();
+})
+
+grid();
+
+/*function grid() {
     let container = document.createElement('div');
     container.id = "main";
     container.className = "container";
@@ -16,4 +65,4 @@ function grid() {
         container.appendChild(row);
     }
     return container;
-}
+}*/
